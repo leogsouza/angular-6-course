7074fc85476dba76a85691466a07535c6237c956
Leonardo Souza <leogsouza@gmail.com>
2018-08-28 18:24:26 -0300

    Populate List of recipes

--- my-recipes-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-recipes-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('First Recipe test', 'Test insert recipe', 'https://pixnio.com/free-images/food-and-drink/barbecue-grilling-food/chicken-grilling-725x544.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
